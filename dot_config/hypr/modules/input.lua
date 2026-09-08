5ab3b817b2c341c71a576e89baf845482c78f3a6
Kasper Scott <kasperscott@gmail.com>
2026-09-08 16:46:33 +0200

    feat: initialize dotfiles

---
+++ dot_config/hypr/modules/input.lua
@@ -0,0 +1,54 @@
+---------------
+---- INPUT ----
+---------------
+
+hl.config({
+    input = {
+        kb_layout    = "se",
+        kb_variant   = "",
+        kb_model     = "",
+        kb_options   = "",
+        kb_rules     = "",
+
+        follow_mouse = 1,
+
+        sensitivity  = 0, -- -1.0 - 1.0, 0 means no modification.
+
+        touchpad     = {
+            natural_scroll = true,
+        },
+    },
+})
+
+local volume_gesture = function(change)
+    local step = math.floor(math.abs(change) + 0.5)
+    if step == 0 then return end
+
+    if change > 0 then
+        -- -l 1.0 strictly limits maximum volume to 100%
+        hl.exec_cmd(string.format("wpctl set-volume -l 1.0 @DEFAULT_AUDIO_SINK@ %d%%+", step))
+    else
+        hl.exec_cmd(string.format("wpctl set-volume @DEFAULT_AUDIO_SINK@ %d%%-", step))
+    end
+end
+
+hl.gesture({
+    fingers = 3,
+    direction = "vertical",
+    action = {
+        start = function(e) volume_gesture(-0.25 * e.delta.y) end,
+        update = function(e) volume_gesture(-0.25 * e.delta.y) end
+    }
+})
+
+hl.gesture({
+    fingers = 3,
+    direction = "horizontal",
+    action = "workspace"
+})
+
+hl.config({
+    gestures = {
+        workspace_swipe_forever = true,
+    }
+})
---
+++ dot_config/hypr/modules/misc.lua
@@ -0,0 +1,11 @@
+----------------
+----  MISC  ----
+----------------
+
+hl.config({
+    misc = {
+        disable_hyprland_logo    = true, -- If true disables the random hyprland logo / anime girl background. :(
+        disable_splash_rendering = true,
+    },
+    xwayland = { force_zero_scaling = true }
+})
